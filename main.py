cfbce8a9a69b74ac40b602fbb3902a4300416433
Tharusha-dev <tharushajayasooriya.dev004@gmail.com>
2024-03-22 21:35:45 +0530

    chunks of 100

--- main.py
+++ main.py
@@ -8,17 +8,17 @@ for l in alphabet:
     print(l)
     letter = l
 
-    with open(f'/by_letter/{l}.txt','r') as file:
+    with open(f'by_letter/{l}.txt','r') as file:
         # print(f'{l}.txt')
         num_of_names = len(file.readlines())
         num_of_files = num_of_names // chunks
         last_chunk = num_of_names % chunks
         
 
-    with open(f'/by_letter/{l}.txt','r') as file:
+    with open(f'by_letter/{l}.txt','r') as file:
 
         count = 1
-        file_name = f'/chunks_of_100/{l}-{count}00.txt'
+        file_name = f'chunks_of_100/{l}-{count}00.txt'
 
         for i in range(num_of_files):
 
@@ -26,7 +26,7 @@ for l in alphabet:
                 for i in range(100):
                     write.write(file.readline())
             count += 1
-            file_name = f'/chunks_of_100/{l}-{count}00.txt'
+            file_name = f'chunks_of_100/{l}-{count}00.txt'
         
         with open(file_name,'w') as write:
             for i in range(last_chunk):
